d9d39b014246d0a6f05d4a5a1556d09540eb49bf
Maru <adduser@Maru>
2024-06-07 12:20:11 +0300

    5

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,23 +1,24 @@
 
 import fs from "fs"; 
 import path from "path";
-import { fileUrlToPath } from "url";
+//import { fileUrlToPath } from "url";
 
-import genDiff from "../src/index.js";
+import { genDiff } from "../src/index.js";
 import test from "node:test";
 
-const __filename = fileUrlToPath(import.meta.url); 
-const __dirname = path.dirname(__filename);
+//const __filename = fileUrlToPath(import.meta.url); 
+//const __dirname = path.dirname(__filename);
 
 function getFixturePath(filePath){
-path.join(__dirname, '..' , '__fixtures__', filePath); 
+return path.join('__tests__', '..' , '__fixtures__', filePath); 
 }
 
 function fileData(filePath){
-    fs.readFileSync(getFixturePath(filePath), 'utf-8');
+  return fs.readFileSync(filePath, 'utf-8');
 }
 
-const referenceFlatFiles = fileData('flat-files-test.txt');
+const referenceFlatFiles = fileData('./__fixtures__/flat-file-test.txt');
+const f = fs.readFileSync('./__fixtures__/flat-file-test.txt', 'utf-8')
 
 test('Flat JSON', () => {
   const file1= getFixturePath('file1.json'); 
