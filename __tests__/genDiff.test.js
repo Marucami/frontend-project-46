ccea3eba1b304d2b55b0535795bc0e2e72f667fc
Marucami <samotskiy2002@mail.ru>
2024-06-09 16:30:39 +0300

    6

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,28 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+// import { fileUrlToPath } from "url";
+import assert from 'assert';
+import { genDiff } from '../src/index.js';
 
-import fs from "fs"; 
-import path from "path";
-//import { fileUrlToPath } from "url";
+// const __filename = fileUrlToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-import { genDiff } from "../src/index.js";
-import test from "node:test";
-
-//const __filename = fileUrlToPath(import.meta.url); 
-//const __dirname = path.dirname(__filename);
-
-function getFixturePath(filePath){
-return path.join('__tests__', '..' , '__fixtures__', filePath); 
+function getFixturePath(filePath) {
+  return path.join('__tests__', '..', '__fixtures__', filePath);
 }
 
-function fileData(filePath){
+function fileData(filePath) {
   return fs.readFileSync(filePath, 'utf-8');
 }
 
 const referenceFlatFiles = fileData('./__fixtures__/flat-file-test.txt');
-const f = fs.readFileSync('./__fixtures__/flat-file-test.txt', 'utf-8')
 
-test('Flat JSON', () => {
-  const file1= getFixturePath('file1.json'); 
-  const file2 = getFixturePath('file2.json');
+describe('genDiff', () => {
+  it('Flat JSON', () => {
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.json');
 
-expect(genDiff(file1, file2)).toEqual(referenceFlatFiles);
-})
+    assert.strictEqual(genDiff(file1, file2), referenceFlatFiles);
+  });
+
+  it('Flat YAML', () => {
+    const file1 = getFixturePath('file1.yml');
+    const file2 = getFixturePath('file2.yml');
+
+    assert.strictEqual(genDiff(file1, file2), referenceFlatFiles);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as fs from 'node:fs';
 import _ from 'lodash';
 import * as path from 'node:path';
 import * as process from 'node:process';
+import yaml from 'js-yaml';
 
 export function dataParse(puti) {
   return JSON.parse(fs.readFileSync(puti, 'utf-8'));
@@ -31,4 +32,26 @@ export function genDiff(file1, file2) {
   }
 
   return diff.join('\n');
+}
+
+export function generateDiff(file1, file2) {
+  const data1 = yaml.parse(file1);
+  const data2 = yaml.parse(file2);
+
+  const diff = {};
+
+  for (const key of keys) {
+    if (data1[key] === data2[key]) {
+      diff.push(`  ${key}: ${data1[key]}`);
+      continue;
+    }
+    if (key in data1) {
+      diff.push(`- ${key}: ${data1[key]}`);
+    }
+    if (key in data2) {
+      diff.push(`+ ${key}: ${data2[key]}`);
+    }
+  }
+
+  return yaml.stringify(diff);
 }
