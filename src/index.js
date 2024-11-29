f0caded1e6a88cf819942746f2e9bb76a921a593
Maru <adduser@Maru>
2024-11-29 14:44:03 +0300

    25

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import * as path from 'path';
 import comparer from './comparator.js';
-import styling from './formatters/index.js';
+import styling from './formatters/compilator.js';
 import parse from './parser.js';
 
 const getFilePath = (fileName) => path.resolve(process.cwd(), fileName);
