d1b63229a4e3de41962f08bdc8e0c6b70be3c45e
Maru <adduser@Maru>
2024-11-29 15:00:21 +0300

    29

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-
+import { program } from 'commander';
 import genDiff from '../src/index.js';
-
-const program = new Command();
 
 program
   .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Comparing of two json-format files with displaying differences.')
   .version('1.0.0')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, options) => {
-    console.log(genDiff(filepath1, filepath2, options.format));
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    const resultDiff = genDiff(filepath1, filepath2, program.opts().format);
+    console.log(resultDiff);
   });
 
-program.parse(process.argv);
+program.parse();
